62c39df55cf5f49453ea0cdf9d8e8b63971d44f2
varmanli <varmanliamirhosein@gmail.com>
2024-09-23 23:22:13 +0330

    fix bug

--- src/components/ui/MobileHeader.jsx
+++ src/components/ui/MobileHeader.jsx
@@ -1,7 +1,7 @@
 
 function MobileHeader() {
   return (
-    <header className="flex flex-col gap-3 md:hidden">
+    <header className="flex flex-col gap-3 pt-4 md:hidden">
       <div className="flex justify-between items-center px-[16px]">
         <div className="flex justify-center items-center gap-5">
           <img
@@ -28,15 +28,15 @@ function MobileHeader() {
       <div className="flex justify-around border-b pb-2 text-[15px] text-[#242424] ">
         <div>
           <p className="font-semibold">Tab 1</p>
-          <span className="bg-[#5B5FC7] w-[120px] h-[2px] absolute left-0 top-[80px] "></span>
+          <span className="bg-[#5B5FC7] w-[33%] h-[2px] absolute left-0 top-[97px] "></span>
         </div>
         <div>
           <p>Tab 2</p>
-          <span className="bg-[#5B5FC7] w-[120px] h-[2px] absolute left-1/3 top-[80px] hidden"></span>
+          <span className="bg-[#5B5FC7] w-[120px] h-[2px] absolute left-1/3 top-[97px]  hidden"></span>
         </div>
         <div>
           <p>Tab 3</p>
-          <span className="bg-[#5B5FC7] w-[120px] h-[2px] absolute left-2/3 top-[80px] hidden"></span>
+          <span className="bg-[#5B5FC7] w-[120px] h-[2px] absolute left-2/3 top-[97px] hidden"></span>
         </div>
       </div>
     </header>
--- src/components/ui/MobileLayout.jsx
+++ src/components/ui/MobileLayout.jsx
@@ -33,32 +33,32 @@ function MobileLayout() {
     <div className="md:hidden w-full">
       <MobileHeader />
       <div className="flex flex-col justify-center items-center gap-4 my-10 w-full mb-20">
-        <Card title={"Chart 1"} description={"Description"}>
+        <Card title={"Chart 1"} description={"Description"} details={true}>
           <SemiCircleChart
             data={semiCirclePercentages}
             config={semiCircleChartOptions}
             tabs={semiCircletabs}
           />
         </Card>
-        <Card title={"Chart 2"} description={"Description"}>
+        <Card title={"Chart 2"} description={"Description"} details={true}>
           <BarChart data={barChartData} options={barChartOptions} />
         </Card>
-        <Card title={"Chart 3"} description={"Description"}>
+        <Card title={"Chart 3"} description={"Description"} details={true}>
           <LineChart chartData={lineChartData} chartConfig={lineChartConfig} />
         </Card>
-        <Card title={"Chart 4"} description={"Description"}>
+        <Card title={"Chart 4"} description={"Description"} details={true}>
           <MultiLayerDoughnutChart
             data={multiLayerDoughnutChartData}
             options={multiLayerDoughnutChartOptions}
           />
         </Card>
-        <Card title={"Chart 5"} description={"Description"}>
+        <Card title={"Chart 5"} description={"Description"} details={true}>
           <DoughnutChart
             dataSets={doughnutChartData}
             colors={doughnutChartColors}
           />
         </Card>
-        <Card title={"Chart 6"} description={"Description"}>
+        <Card title={"Chart 6"} description={"Description"} details={false}>
           <MultiBarChart
             tabsData={multiChartTabsData}
             options={multiLayerDoughnutChartOptions}
